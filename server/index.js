f8b4b94c07ee3a9c406d460aa2b0a6b656300589
bybycool <idriceanu.stefan@gmail.com>
2016-02-20 03:41:34 +0100

    Making global sql queries, fix

--- server/index.js
+++ server/index.js
@@ -2,33 +2,40 @@
 var moduleexample = require("./lib/module-example.js");
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+		host     : "localhost",
+		user     : "root",
+		password : "mamaligos",
+		database : "skybook"
+});
+
+connection.connect(function(err){
+	if(!err) {
+	    console.log("Database is connected ... nn");    
+	} else {
+	    console.log("Error connecting database ... nn");    
+	}
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-  	console.log('Example app listening on port 3000!');
-	//login to that database
-	var mysql      = require('mysql');
-	var connection = mysql.createConnection({
-  		host     : "localhost",
-  		user     : "root",
-  		password : "mamaligos",
-  		database : "skybook"
-	});
-	connection.connect();
+	console.log('Example app listening on port 3000!');
 
 	connection.query("select * from user",function(err,rows){
-       	rows = rows[0];
-        if(!err) {
-            console.log(rows.name);
-        }           
-    });
+	   	rows = rows[0];
+	    if(!err) {
+	        console.log(rows.name);
+	    }           
+	    
+	});
 
-  //This is just an example of how modules work 
-  //IGNORE !!!
-  var text = moduleexample.sayHello();
-  console.log(text);
+  	//This is just an example of how modules work 
+  	//IGNORE !!!
+  	var text = moduleexample.sayHello();
+  	console.log(text);
 });
